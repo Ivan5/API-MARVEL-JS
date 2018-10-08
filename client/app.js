1ac7084d424568ff5e7a080973b92054ec39a0e3
Ivan5 <converse_ivan@hotmail.com>
2018-10-08 16:46:50 -0500

    Add function to 'kill' the characters

--- client/app.js
+++ client/app.js
@@ -1,4 +1,5 @@
 const charactersElemnt = document.querySelector('.characters');
+const thanosElement = document.querySelector('#thanos');
 
 const charactersURL = 'https://gateway.marvel.com:443/v1/public/events/29/characters?limit=100&apikey=63536fc3fd34892b6fc1c44c5ca69989';
 
@@ -28,6 +29,8 @@ function addCharactersToPage(characterData){
       //console.log(result.thumbnail.path + '/standard_medium.jpg');
       const characterImage = result.thumbnail.path + '/standard_medium.jpg';
       const characterElemnt = document.createElement('div');
+      characterElemnt.style.transform = 'scale(1)';
+      characterElemnt.className = 'character';
 
       const imageElement = document.createElement('img');
       imageElement.src = characterImage;
@@ -47,3 +50,39 @@ function addCharactersToPage(characterData){
 
 getCharacterData()
   .then(addCharactersToPage);
+
+
+thanosElement.addEventListener('click', () => {
+  setTimeout(() => {
+    balanceUniverse();
+  },2000);
+});
+
+
+function balanceUniverse(){
+  const characters = [...document.querySelectorAll('.character')];
+
+  let leftToDie = Math.floor(characters.length / 2);
+
+  kill(characters, leftToDie);
+}
+
+function kill(characters, leftToDie){
+  if(leftToDie > 0){
+    //do the killing
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    const [characterChosen] = characters.splice(randomIndex, 1);
+    characterChosen.style.transform = 'scale(0)';
+    characterChosen.classList.remove('alive');
+    characterChosen.classList.add('dead');
+
+    setTimeout(() => {
+      kill(characters, leftToDie - 1);
+    }, 1000)
+  }else{
+    document.querySelectorAll('.dead').forEach(character => {
+      character.style.height = '0px';
+      character.style.width = '0px';
+    });
+  }
+}
