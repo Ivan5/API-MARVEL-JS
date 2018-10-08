9bb77ac68f7bc12d0613b1851c3b590e6b08d707
Ivan5 <converse_ivan@hotmail.com>
2018-10-07 19:10:23 -0500

    Add Image characters in the page

--- client/app.js
+++ client/app.js
@@ -1,7 +1,24 @@
 const charactersURL = 'https://gateway.marvel.com:443/v1/public/events/29/characters?limit=100&apikey=63536fc3fd34892b6fc1c44c5ca69989';
 
-fetch(charactersURL)
+function getCharacterData(){
+  if(localStorage.characterData){
+    return Promise.resolve(JSON.parse(localStorage.characterData));
+  }
+  return fetch(charactersURL)
   .then(response => response.json())
   .then(data => {
-    console.log(data.data.results);
-  })
+    localStorage.characterData = JSON.stringify(data);
+    return data;
+  });
+}
+
+function addCharactersToPage(characterData){
+  //console.log(characterData.data.results);
+  characterData.data.results.forEach(result => {
+    console.log(result.thumbnail.path + '/standard_medium.jpg');
+    const characterImage = result.thumbnail.path + '/standard_medium.jpg';
+  });
+}
+
+getCharacterData()
+  .then(addCharactersToPage);
